b6fac81a5aff75fe99885bf9dcdb29ea71eb3323
Chandler <chandler.ryan@live.com>
2020-03-17 17:11:45 -0500

    footer tweaks

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
-<?php if(!is_front_page() && ! is_page('contact') ) : ?>
+<?php 
+$pageNoSidebar = array(
+  'contact', 'about', 'projects', 'about-me', 'about-the-portfolio', 'todo-items-on-the-portfolio'
+);
+if(!is_front_page() && ! is_page('contact') ) : ?>
     <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
       <?php if(is_active_sidebar('sidebar')): ?>
         <?php dynamic_sidebar('sidebar'); ?>
@@ -9,6 +13,12 @@
 </div><!-- /.container -->
     <footer class="blog-footer">
       <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></p>
+      <h2 class="text-center social">
+                <a href="https://www.facebook.com/chandler.ryan.543" target="_blank"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.linkedin.com/in/chandler-ryan-91b094143/" target="_blank"><i class="fa fa-linkedin"></i></a>
+                <a href="https://github.com/Shakeyone" target="_blank"><i class="fa fa-github"></i></a>
+                <a href="#" target="_blank"><i class="fa fa-bicycle"></i></a>
+            </h2>
       <p>
         <a href="#"><i class="fa fa-arrow-circle-up" aria-hidden="true"></i>Back to top</a>
       </p>
