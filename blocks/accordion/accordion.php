858a65e8b36a6b02fdfdc23d9beb6ebefa60d4f9
Chandler <chandler.ryan@live.com>
2020-03-17 14:39:11 -0500

    axxordion update

--- blocks/accordion/accordion.php
+++ blocks/accordion/accordion.php
@@ -1,3 +1,26 @@
+
+<?php
+
+/**
+ * Accordion Block Template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+
+    if ( is_admin() ):?>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<?php 
+    endif;
+    $accordion = get_field( 'accordion' );
+    echo( '<pre>' . print_r( $accordion, true) . '</pre>');
+
+?>
+
 <div id="accordion">
   <div class="card">
     <div class="card-header" id="headingOne">
