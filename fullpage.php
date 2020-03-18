6d57d9ecb315c272acb48d747c64cef47eaf12ec
Chandler <chandler.ryan@live.com>
2020-03-17 20:28:24 -0500

    fullpage style and template tweaks

--- fullpage.php
+++ fullpage.php
@@ -5,18 +5,8 @@
 */
 get_header(); ?>
   <div class="row">
-    <div class="col blog-main">
-      <?php if(have_posts()) : ?>
-        <?php while(have_posts()) : the_post(); ?>
-          <div class="blog-post">
-            <h2 class="blog-post-title">
-              <?php the_title(); ?>
-            </h2>
-            <?php the_content(); ?>
-          </div><!-- /.blog-post -->
-        <?php endwhile; ?>
-      <?php else : ?>
-        <p><?php __('No Page Found'); ?></p>
-      <?php endif; ?>          
-    </div><!-- /.blog-main -->
+    <div class="col page-main">
+        <h1 class="blog-post-title"><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+    </div><!-- /.page-main -->
 <?php get_footer(); ?>
