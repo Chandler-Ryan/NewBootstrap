22e3dfa4c5fd99e08c9494afe85fb70c36a83ec6
Chandler <chandler.ryan@live.com>
2020-03-19 12:18:09 -0500

    front page to use shared footer

--- footer.php
+++ footer.php
@@ -5,9 +5,11 @@ if(!is_front_page() &&  is_page_template( 'Full Width Page' ) ) : ?>
         <?php dynamic_sidebar('sidebar'); ?>
       <?php endif; ?>
     </div><!-- /.blog-sidebar -->
-<?php endif; ?>
+<?php endif;
+if(!is_front_page() ):?>
   </div><!-- /.row -->
 </div><!-- /.container -->
+<?php endif;?>
     <footer class="blog-footer">
       <p>&copy; <?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></p>
       <h2 class="text-center social">
@@ -23,6 +25,18 @@ if(!is_front_page() &&  is_page_template( 'Full Width Page' ) ) : ?>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <?php wp_footer(); ?>
+    <?php if( is_front_page() ): ?>
+    <script>
+      $(document).ready(function(){
+          var height = 0;
+          $('.box').each(function(){
+            if ($(this).height() > height) height = $(this).height();
+          });
+          $('.box').each(function(){
+            $(this).height(height);
+          });
+      });
+    </script>
+    <?php endif; wp_footer(); ?>
   </body>
 </html>
